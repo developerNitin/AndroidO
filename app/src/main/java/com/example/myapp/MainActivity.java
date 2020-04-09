da2c6fa356e0725625fb7dfe793e45baef3a6b11
Nitin Birdi <nitin.birdi@icloud.com>
2020-04-09 16:48:15 +0530

    button added

--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -3,8 +3,14 @@ package com.example.myapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void buttonClicked(View view) {
+        Log.i("info", "Hello World");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
