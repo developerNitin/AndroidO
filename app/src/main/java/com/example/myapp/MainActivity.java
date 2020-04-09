643f0fa6ffd2cd6119b1cc018def36f8df603cf4
Nitin Birdi <nitin.birdi@icloud.com>
2020-04-09 18:06:17 +0530

    PopUpM

--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -6,15 +6,17 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText nameEditText = (EditText) findViewById(R.id.nameEditText);
-    EditText passworEditText = (EditText) findViewById(R.id.passwordEditText);
+    EditText passworEditText = (EditText) findViewById(R.id.passwordTextField);
 
     public void buttonClicked(View view) {
         Log.i("info", nameEditText.getText().toString());
         Log.i("passord", passworEditText.getText().toString());
+        Toast.makeText( this , "Hello " + nameEditText.getText().toString(), Toast.LENGTH_SHORT).show();
     }
 
     @Override
